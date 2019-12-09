1bbeddd7fe7596852afb5d86a001b4272d125998
Julius J. Tyner <ColonelDuddits@server.fake>
2019-12-09 08:41:27 -0500

    Interim commit

---
+++ EPPlus/Xml/ExcelSingleXmlCell.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OfficeOpenXml.Xml
+{
+    public class ExcelSingleXmlCell : IDisposable
+    {
+
+        #region IDisposable Support
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+
+        }
+        #endregion
+    }
+}
